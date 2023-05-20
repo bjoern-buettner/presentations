9c913c457ef288a8f8ae5669c6ad74ef69bfebbf
Björn Büttner <bjoern.buettner@jtl-software.com>
2023-05-20 14:13:25 +0200

    minify content combine css files combine js files

--- src/build.js
+++ src/build.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 const yaml = require('yaml');
 const handlebars = require("handlebars");
+const cssc = require('css-condense');
+const { minify } = require('html-minifier-terser');
+
 const page = handlebars.compile(fs.readFileSync(`${__dirname}/../src/page.html`, 'utf8'));
 const index = handlebars.compile(fs.readFileSync(`${__dirname}/../src/index.html`, 'utf8'));
+const htmlMinifyOptions = {
+  collapseBooleanAttributes: true,
+  collapseWhitespace: true,
+  removeComments: true,
+  useShortDoctype: true,
+};
 
 if (fs.existsSync(`${__dirname}/../public`)) {
   fs.rmdirSync(`${__dirname}/../public`, { recursive: true });
@@ -17,23 +26,58 @@ for (const file of fs.readdirSync(`${__dirname}/../presentations`)) {
       title: presentation.title,
       name,
     });
-    fs.writeFileSync(`${__dirname}/../public/${name}.html`, page({presentation}));
+    (async () => {
+      fs.writeFileSync(
+          `${__dirname}/../public/${name}.html`,
+          await minify(page({presentation}), htmlMinifyOptions)
+      );
+    })();
   }
 }
 
-fs.writeFileSync(`${__dirname}/../public/index.html`, index({pages}));
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js/plugin/highlight/highlight.js`, `${__dirname}/../public/highlight.js`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js/plugin/highlight/monokai.css`, `${__dirname}/../public/monokai.css`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js/plugin/notes/notes.js`, `${__dirname}/../public/notes.js`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js/plugin/notes/speaker-view.html`, `${__dirname}/../public/speaker-view.html`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js-verticator/plugin/verticator/verticator.js`, `${__dirname}/../public/verticator.js`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js-verticator/plugin/verticator/verticator.css`, `${__dirname}/../public/verticator.css`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js-copycode/plugin/copycode/copycode.js`, `${__dirname}/../public/copycode.js`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js-copycode/plugin/copycode/copycode.css`, `${__dirname}/../public/copycode.css`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js/dist/reveal.js`, `${__dirname}/../public/reveal.js`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js/dist/reset.css`, `${__dirname}/../public/reset.css`);
-fs.copyFileSync(`${__dirname}/../node_modules/reveal.js/dist/reveal.css`, `${__dirname}/../public/reveal.css`);
-fs.copyFileSync(`${__dirname}/../src/bjoern-buettner-theme.css`, `${__dirname}/../public/bjoern-buettner-theme.css`);
-fs.copyFileSync(`${__dirname}/../src/style.css`, `${__dirname}/../public/style.css`);
+(async () => {
+  fs.writeFileSync(
+      `${__dirname}/../public/index.html`,
+      await minify(index({pages}), htmlMinifyOptions)
+  );
+})();
+
+(async () => {
+  fs.writeFileSync(
+      `${__dirname}/../public/speaker-view.html`,
+      await minify(fs.readFileSync(`${__dirname}/../node_modules/reveal.js/plugin/notes/speaker-view.html`, 'utf8'), htmlMinifyOptions)
+  );
+})();
+
 fs.copyFileSync(`${__dirname}/../src/logo.png`, `${__dirname}/../public/logo.png`);
 fs.copyFileSync(`${__dirname}/../src/favicon.ico`, `${__dirname}/../public/favicon.ico`);
+
+((cssFiles) => {
+  let css = '';
+  for (const file of cssFiles) {
+    css = css + `\n/*!${file.split('/').pop()}*/\n` + cssc.compress(fs.readFileSync(file, 'utf8'));
+  }
+  fs.writeFileSync(`${__dirname}/../public/style.css`, css.substring(1));
+})([
+  `${__dirname}/../node_modules/reveal.js/dist/reset.css`,
+  `${__dirname}/../node_modules/reveal.js/dist/reveal.css`,
+  `${__dirname}/../src/bjoern-buettner-theme.css`,
+  `${__dirname}/../node_modules/reveal.js/plugin/highlight/monokai.css`,
+  `${__dirname}/../node_modules/reveal.js-verticator/plugin/verticator/verticator.css`,
+  `${__dirname}/../node_modules/reveal.js-copycode/plugin/copycode/copycode.css`,
+  `${__dirname}/../src/style.css`,
+]);
+
+((jsFiles) => {
+  let js = '';
+  for (const file of jsFiles) {
+    js = js + `\n/*!${file.split('/').pop()}*/\n` + fs.readFileSync(file, 'utf8');
+  }
+  fs.writeFileSync(`${__dirname}/../public/scripts.js`, js.substring(1));
+})([
+  `${__dirname}/../node_modules/reveal.js/dist/reveal.js`,
+  `${__dirname}/../node_modules/reveal.js/plugin/highlight/highlight.js`,
+  `${__dirname}/../node_modules/reveal.js/plugin/notes/notes.js`,
+  `${__dirname}/../node_modules/reveal.js-verticator/plugin/verticator/verticator.js`,
+  `${__dirname}/../node_modules/reveal.js-copycode/plugin/copycode/copycode.js`,
+]);
